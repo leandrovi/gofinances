3ad16aeef429f0e59e3fede09103cf9816876b45
leandrovi <leandrofernandesvieira@gmail.com>
2020-04-23 22:51:01 -0300

    feat: finish challenge

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,11 +1,9 @@
 import { Router } from 'express';
 
 import transactionsRouter from './transactions.routes';
-import categoriesRouter from './categories.routes';
 
 const routes = Router();
 
 routes.use('/transactions', transactionsRouter);
-routes.use('/categories', categoriesRouter);
 
 export default routes;
--- backend/src/routes/transactions.routes.ts
+++ backend/src/routes/transactions.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
 import { getCustomRepository } from 'typeorm';
 
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
-// import DeleteTransactionService from '../services/DeleteTransactionService';
-// import ImportTransactionsService from '../services/ImportTransactionsService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
+import ImportTransactionsService from '../services/ImportTransactionsService';
+
+import uploadConfig from '../config/upload';
+
+const upload = multer(uploadConfig);
 
 const transactionsRouter = Router();
 
@@ -41,12 +46,26 @@ transactionsRouter.post('/', async (request, response) => {
   return response.json(transaction);
 });
 
-// transactionsRouter.delete('/:id', async (request, response) => {
-//   // TODO
-// });
+transactionsRouter.delete('/:id', async (request, response) => {
+  const { id } = request.params;
 
-// transactionsRouter.post('/import', async (request, response) => {
-//   // TODO
-// });
+  const deleteTransaction = new DeleteTransactionService();
+
+  await deleteTransaction.execute(id);
+
+  return response.status(204).send();
+});
+
+transactionsRouter.post(
+  '/import',
+  upload.single('file'),
+  async (request, response) => {
+    const importTransactions = new ImportTransactionsService();
+
+    const transactions = await importTransactions.execute(request.file.path);
+
+    return response.json(transactions);
+  },
+);
 
 export default transactionsRouter;
